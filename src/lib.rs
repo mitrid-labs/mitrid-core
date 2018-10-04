6ba4ac9005dceeff3c9d27cf2f131b437a781f19
Christian Nyumbayire <chritchens@gmail.com>
2018-10-04 20:02:26 +0200

    Fix the lib.rs doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # Mitrid Core
 //!
-//! `mitrid-core` provides the core traits and types used by the Mitrid framework, a framework
+//! `mitrid_core` provides the core traits and types used by the Mitrid framework, a framework
 //! for building blockchains and other distributed ledgers using block-based authenticated
 //! data structures (authenticated trees, authenticated graphs, etc).
 
