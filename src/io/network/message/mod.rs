d01d989082c9b181a9cf785b6f62daee967fdb2d
Christian Nyumbayire <chritchens@gmail.com>
2018-10-22 10:23:57 +0200

    Document the message mod file and add the message empty file

--- src/io/network/message/mod.rs
+++ src/io/network/message/mod.rs
@@ -1,3 +1,15 @@
+//! # Message
+//!
+//! `message` is the module providing the types used for network messages.
+
+/// Type used to represent a network method.
 pub mod method;
+
+/// Type used to represent a network resource.
 pub mod resource;
+
+/// Type used to represent a network message data.
 pub mod message_data;
+
+/// Type used to represent a network message.
+pub mod message;
