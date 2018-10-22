756058efe80fb813220e1f3fdd5d5ef409949ff8
Christian Nyumbayire <chritchens@gmail.com>
2018-10-22 09:53:38 +0200

    Move transport in its module

--- src/io/network/network.rs
+++ src/io/network/network.rs
@@ -24,27 +24,6 @@ use io::Permission;
 use io::Session;
 use io::Node;
 
-/// Trait implemented by network transports.
-pub trait Transport<A, D>
-    where   A: Datable + VariableSize,
-            D: Datable + Serializable
-{
-    /// Opens a connection to a network address.
-    fn connect<P: Datable>(&mut self, params: &P, address: &A) -> Future<()>;
-
-    /// Closes a connection to a network address.
-    fn disconnect<P: Datable>(&mut self, params: &P, address: &A) -> Future<()>;
-
-    /// Listen to connections incoming from a network address.
-    fn listen<P: Datable>(&mut self, params: &P, address: &A) -> Future<()>;
-
-    /// Sends data through a network connection.
-    fn send<P: Datable>(&mut self, params: &P, data: &D) -> Future<()>;
-
-    /// Receives data from a network connection.
-    fn recv<P: Datable>(&mut self, params: &P) -> Future<D>;
-}
-
 pub enum Method {
     Ping,
     Session,
