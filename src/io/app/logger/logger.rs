c4be2f4651f79e839bd3536856113fdd53e1cefa
Christian Nyumbayire <chritchens@gmail.com>
2018-10-25 18:12:22 +0200

    Add logger

--- src/io/app/logger/logger.rs
+++ src/io/app/logger/logger.rs
@@ -1,3 +1,21 @@
 //! # Logger
 //!
-//! `logger` is the module providing the trait implemented by types used by I/O applications to log.
+//! `logger` is the module providing the trait implemented by types that can log to stdout/stderr.
+
+use base::Result;
+use io::app::logger::LogLevel;
+
+/// Trait implemented by types that can log to stdout/stderr.
+pub trait Logger {
+    fn log_level(&self) -> LogLevel;
+
+    fn log_error(&self, content: &str) -> Result<()>;
+
+    fn log_warn(&self, content: &str) -> Result<()>;
+    
+    fn log_info(&self, content: &str) -> Result<()>;
+    
+    fn log_debug(&self, content: &str) -> Result<()>;
+    
+    fn log_trace(&self, content: &str) -> Result<()>;
+}
--- src/io/app/logger/mod.rs
+++ src/io/app/logger/mod.rs
@@ -1,9 +1,12 @@
 //! # Logger
 //!
-//! `logger` is the module providing the traits and types used to by I/O applications for logging.
+//! `logger` is the module providing the traits and types used for logging to stdout/stderr.
 
 /// Type used to represent the log level.
 pub mod log_level;
 
-/// Trait implemented by types used by I/O applications to log.
+/// Trait implemented by types used to log to stdout/stderr.
 pub mod logger;
+
+pub use self::log_level::LogLevel;
+pub use self::logger::Logger;
