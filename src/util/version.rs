bfd5f86cfbd33f4da1c96ca97c4827545c9a1788
Christian Nyumbayire <chritchens@gmail.com>
2018-11-08 16:10:54 +0100

    Bump up to v0.8.0

--- src/util/version.rs
+++ src/util/version.rs
@@ -13,7 +13,7 @@ use base::Serializable;
 use util::regex;
 
 /// Current Semver version of the library.
-pub const VERSION: &str = "0.7.2";
+pub const VERSION: &str = "0.8.0";
 
 /// Regex pattern of a numeric version.
 pub const NUMERIC_VERSION: &str = "^[0-9]*$";
