27d36fe717300f1b361aad53e2296b259cbb0721
Christian Nyumbayire <chritchens@gmail.com>
2018-11-12 18:09:52 +0100

    Remove Any in Datable

--- src/base/data.rs
+++ src/base/data.rs
@@ -9,7 +9,7 @@ use base::check::Checkable;
 use base::size::Sizable;
 
 /// Trait implemented by generic types that can be used as fields or parameters.
-pub trait Datable : 'static + Any + Send + Sync + Sized + Clone + Eq + Debug + Default + Sizable + Checkable {}
+pub trait Datable : 'static + Send + Sync + Sized + Clone + Eq + Debug + Default + Sizable + Checkable {}
 
 impl Datable for () {}
 
